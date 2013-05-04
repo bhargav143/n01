afad66aa14a27c522662fa56d26668332e2b9f61
Bhargav Achary <bhargav_achary@yahoo.com>
2013-05-05 04:35:41 +0530

    init
    initial commit

---
+++ n01/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace n01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupSimpleInt = new System.Windows.Forms.GroupBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNegate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.tbxNegation = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxNumber_last = new System.Windows.Forms.TextBox();
+            this.tbxLast = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupSimpleInt.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Principal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rate";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(121, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Simple Interest";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(121, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // groupSimpleInt
+            // 
+            this.groupSimpleInt.Controls.Add(this.btnCalc);
+            this.groupSimpleInt.Controls.Add(this.label1);
+            this.groupSimpleInt.Controls.Add(this.label4);
+            this.groupSimpleInt.Controls.Add(this.textBox1);
+            this.groupSimpleInt.Controls.Add(this.textBox4);
+            this.groupSimpleInt.Controls.Add(this.textBox2);
+            this.groupSimpleInt.Controls.Add(this.label3);
+            this.groupSimpleInt.Controls.Add(this.label2);
+            this.groupSimpleInt.Controls.Add(this.textBox3);
+            this.groupSimpleInt.Location = new System.Drawing.Point(28, 26);
+            this.groupSimpleInt.Name = "groupSimpleInt";
+            this.groupSimpleInt.Size = new System.Drawing.Size(227, 239);
+            this.groupSimpleInt.TabIndex = 8;
+            this.groupSimpleInt.TabStop = false;
+            this.groupSimpleInt.Text = "Simple Interest";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(77, 190);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNegate);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbxNumber);
+            this.groupBox1.Controls.Add(this.tbxNegation);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(272, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 239);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Negation";
+            // 
+            // btnNegate
+            // 
+            this.btnNegate.Location = new System.Drawing.Point(77, 190);
+            this.btnNegate.Name = "btnNegate";
+            this.btnNegate.Size = new System.Drawing.Size(75, 23);
+            this.btnNegate.TabIndex = 8;
+            this.btnNegate.Text = "Negate";
+            this.btnNegate.UseVisualStyleBackColor = true;
+            this.btnNegate.Click += new System.EventHandler(this.btnNegate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Number";
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Location = new System.Drawing.Point(121, 80);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbxNumber.TabIndex = 0;
+            // 
+            // tbxNegation
+            // 
+            this.tbxNegation.Location = new System.Drawing.Point(121, 106);
+            this.tbxNegation.Name = "tbxNegation";
+            this.tbxNegation.Size = new System.Drawing.Size(100, 20);
+            this.tbxNegation.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Negation";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLast);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.tbxNumber_last);
+            this.groupBox2.Controls.Add(this.tbxLast);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(519, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(227, 239);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Units Digit";
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(77, 190);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 8;
+            this.btnLast.Text = "Last Digit";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Number";
+            // 
+            // tbxNumber_last
+            // 
+            this.tbxNumber_last.Location = new System.Drawing.Point(121, 80);
+            this.tbxNumber_last.Name = "tbxNumber_last";
+            this.tbxNumber_last.Size = new System.Drawing.Size(100, 20);
+            this.tbxNumber_last.TabIndex = 0;
+            // 
+            // tbxLast
+            // 
+            this.tbxLast.Location = new System.Drawing.Point(121, 106);
+            this.tbxLast.Name = "tbxLast";
+            this.tbxLast.Size = new System.Drawing.Size(100, 20);
+            this.tbxLast.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Last Digit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 397);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupSimpleInt);
+            this.Name = "Form1";
+            this.Text = "First Form";
+            this.groupSimpleInt.ResumeLayout(false);
+            this.groupSimpleInt.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupSimpleInt;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNegate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.TextBox tbxNegation;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxNumber_last;
+        private System.Windows.Forms.TextBox tbxLast;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ n01/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace n01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double p = double.Parse(textBox1.Text);
+            double t = double.Parse(textBox2.Text);
+            double r = double.Parse(textBox3.Text);
+            double si = p * t * r / 100;
+            textBox4.Text = si + "";
+        }
+
+        private void btnNegate_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(tbxNumber.Text);
+            int n = x * (-1);
+            tbxNegation.Text = n + "";
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            long x = long.Parse(tbxNumber_last.Text);
+            long l = x % 10;
+            tbxLast.Text = l + "";
+
+        }
+
+        
+
+       
+
+    }
+}
